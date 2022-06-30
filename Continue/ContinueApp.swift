f6a9bf0d1989802ad437cdb4ccdf908d993e9fbc
Wongi <hpurs95@gmail.com>
2022-07-01 00:06:00 +0900

    Initial Commit

---
+++ Continue/ContinueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinueApp.swift
+//  Continue
+//
+//  Created by MacBook Air on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ContinueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
